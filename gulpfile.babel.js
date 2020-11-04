8d05e3ddbd66d93af5bb0d4c77868f1703cdea3e
alaced <58386621+alaced@users.noreply.github.com>
2020-11-04 18:31:13 +0900

    project01
    grid, scss, responsive stuff

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,17 +6,25 @@ import autoprefixer from "gulp-autoprefixer";
 
 sass.compiler = require("node-sass");
 
-const routes = {
+const routes01 = {
   css: {
-    watch: "src/scss/*",
-    src: "src/scss/styles.scss",
-    dest: "dist/css",
+    watch: "project01/src/scss/*",
+    src: "project01/src/scss/styles.scss",
+    dest: "project01/dist/css",
   },
 };
 
-const styles = () =>
+const routes02 = {
+  css: {
+    watch: "project02/src/scss/*",
+    src: "project02/src/scss/styles.scss",
+    dest: "project02/dist/css",
+  },
+};
+
+const styles01 = () =>
   gulp
-    .src(routes.css.src)
+    .src(routes01.css.src)
     .pipe(sass().on("error", sass.logError))
     .pipe(
       autoprefixer({
@@ -25,18 +33,42 @@ const styles = () =>
       })
     )
     .pipe(minify())
-    .pipe(gulp.dest(routes.css.dest));
+    .pipe(gulp.dest(routes01.css.dest));
 
-const watch = () => {
-  gulp.watch(routes.css.watch, styles);
+const styles02 = () =>
+  gulp
+    .src(routes02.css.src)
+    .pipe(sass().on("error", sass.logError))
+    .pipe(
+      autoprefixer({
+        flexbox: true,
+        grid: "autoplace",
+      })
+    )
+    .pipe(minify())
+    .pipe(gulp.dest(routes02.css.dest));
+
+const watch01 = () => {
+  gulp.watch(routes01.css.watch, styles01);
 };
 
-const clean = () => del(["dist/"]);
+const watch02 = () => {
+  gulp.watch(routes02.css.watch, styles02);
+};
 
-const prepare = gulp.series([clean]);
+const clean01 = () => del(["project01/dist/"]);
+const clean02 = () => del(["project02/dist/"]);
 
-const assets = gulp.series([styles]);
+const prepare01 = gulp.series([clean01]);
+const prepare02 = gulp.series([clean02]);
 
-const live = gulp.parallel([watch]);
+const assets01 = gulp.series([styles01]);
+const assets02 = gulp.series([styles02]);
 
-export const dev = gulp.series([prepare, assets, live]);
+const live01 = gulp.parallel([watch01]);
+const live02 = gulp.parallel([watch02]);
+
+export const dev = gulp.series(
+  // [prepare01, assets01, live01]
+  [prepare02, assets02, live02]
+);
